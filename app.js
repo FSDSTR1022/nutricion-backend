a93c81a6c62411ee390482c0d471ac66529c1bfc
Federico Gómez <federicogomez.ar@gmail.com>
2023-01-20 17:17:31 +0100

    controlador y modelos de ejercicos

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const cors = require('cors')
+//const cors = require('cors')
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
-const routes = require('./routes/index')
+//const routes = require('./routes/index')
 
 var app = express();
 
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors())
+//app.use(cors())
 
 //app.use('/', indexRouter);
 //app.use('/users', usersRouter);
