eb9d73bc8bef7edd4116a6234249ea157d740d68
Federico Gómez <federicogomez.ar@gmail.com>
2023-02-27 20:35:40 +0100

    Corrección bug en rutas de rutinas

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
+router.get('/all', userController.getAllUsers);
 router.get('/:id', userController.getUsersByProfessional);
 router.post('/', userController.createUser);
 router.get('/user/:id', userController.getUserbyID);
 router.put('/:id', userController.updateUser);
-router.get('/all', userController.getAllUsers);
+
 
 router.post('/login', userController.loginUser);
 
