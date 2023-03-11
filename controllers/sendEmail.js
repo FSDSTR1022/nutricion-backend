77efa01716214852a75b74a50e2d31b8013a292d
Hector Perez <mstrhache@gmail.com>
2023-03-11 17:31:46 +0000

    corrección anterior

--- controllers/sendEmail.js
+++ controllers/sendEmail.js
@@ -5,53 +5,52 @@ const api =
 
 SibApiV3Sdk.ApiClient.instance.authentications['api-key'].apiKey = api;
 
-const sendEmail = patient => {+
+const sendEmail = patient => {
 	if (patient.userType != 'profesional') {
-			new SibApiV3Sdk.TransactionalEmailsApi()
-				.sendTransacEmail({
-					subject: 'Bienvenid@a Nutrición Pro!',
-					sender: { email: 'nutricionpropro@gmail.com', name: 'Nutrición Pro' },
-					templateId:1,
-					replyTo: { email: 'nutricionpropro@gmail.com', name: 'Nutrición Pro' },
-					to: [{ name: patient.name, email: patient.email }],
-					
-					params: {
-						bodyName: patient.name,
-						bodyEmail: patient.email,
-						bodyPass: patient.password,
-					},
-				})
-				.then(
-					function (data) {
-						console.log(data);
-					},
-					function (error) {
-						console.error(error);
-					}
-				);
-			} else {
-			new SibApiV3Sdk.TransactionalEmailsApi()
-				.sendTransacEmail({
-					subject: 'Bienvenid@a Nutrición Pro!',
-					sender: { email: 'nutricionpropro@gmail.com', name: 'Nutrición Pro' },
-					templateId:3,
-					replyTo: { email: 'nutricionpropro@gmail.com', name: 'Nutrición Pro' },
-					to: [{ name: patient.name, email: patient.email }],
-					params: {
-						bodyName: patient.name,
-						bodyEmail: patient.email,
-						bodyPass: patient.password,
-					},
-				})
-				.then(
-					function (data) {
-						console.log(data);
-					},
-					function (error) {
-						console.error(error);
-					}
-				);
+		new SibApiV3Sdk.TransactionalEmailsApi()
+			.sendTransacEmail({
+				subject: 'Bienvenid@a Nutrición Pro!',
+				sender: { email: 'nutricionpropro@gmail.com', name: 'Nutrición Pro' },
+				templateId: 1,
+				replyTo: { email: 'nutricionpropro@gmail.com', name: 'Nutrición Pro' },
+				to: [{ name: patient.name, email: patient.email }],
 
-	}			
+				params: {
+					bodyName: patient.name,
+					bodyEmail: patient.email,
+					bodyPass: patient.password,
+				},
+			})
+			.then(
+				function (data) {
+					console.log(data);
+				},
+				function (error) {
+					console.error(error);
+				}
+			);
+	} else {
+		new SibApiV3Sdk.TransactionalEmailsApi()
+			.sendTransacEmail({
+				subject: 'Bienvenid@a Nutrición Pro!',
+				sender: { email: 'nutricionpropro@gmail.com', name: 'Nutrición Pro' },
+				templateId: 3,
+				replyTo: { email: 'nutricionpropro@gmail.com', name: 'Nutrición Pro' },
+				to: [{ name: patient.name, email: patient.email }],
+				params: {
+					bodyName: patient.name,
+					bodyEmail: patient.email,
+					bodyPass: patient.password,
+				},
+			})
+			.then(
+				function (data) {
+					console.log(data);
+				},
+				function (error) {
+					console.error(error);
+				}
+			);
+	}
 };
 module.exports = sendEmail;
