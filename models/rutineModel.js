bbe02703543d7690f84dfb06b8c46b4df8831e06
Hector Perez <mstrhache@gmail.com>
2023-03-14 10:56:28 +0000

    string professinal

--- models/rutineModel.js
+++ models/rutineModel.js
@@ -9,11 +9,7 @@ const RutineSchema = new Schema(
 			ref: 'user',
 		},
 		day: Date,
-<<<<<<< HEAD
-		professional: Schema.Types.ObjectId,
-=======
-		status:String,
->>>>>>> 77dd0e86c29deffb3bdf5305590ba53375c7b108
+		professional: String,
 		rounds: [
 			{
 				order: Number,
